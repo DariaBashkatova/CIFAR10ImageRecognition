ff2943faa098273be418d661d5c24e9cdebcdc25
ethanp56 <ethanp56@yahoo.com>
2016-03-22 14:22:17 -0500

    Added basic implementation of neural network using sklearn

--- run_neural_network.py
+++ run_neural_network.py
@@ -1,29 +1,14 @@
 import numpy as np
 import utils
-from sklearn import cross_validation
+from sklearn import model_selection
 from sklearn.metrics import confusion_matrix
-from sklearn.naive_bayes import GaussianNB
-from sklearn.naive_bayes import MultinomialNB
-from sklearn.naive_bayes import BernoulliNB
+from sklearn.neural_network import MLPClassifier
 
 
 
 # Initialize variables
 FINAL_RUN = False
-NAIVE_BAYES_TYPE = "B"
-
-if NAIVE_BAYES_TYPE == "B":
-	nb = BernoulliNB()
-	bins = 2
-elif NAIVE_BAYES_TYPE == "G":
-	nb = GaussianNB()
-	bins = False
-elif NAIVE_BAYES_TYPE == "M":
-	nb = MultinomialNB()
-	bins = 256
-else:
-	print "No such NAIVE_BAYES_TYPE"
-	assert(False)
+bins = False
 
 class_to_value_mapping = {"airplane": 0, "automobile": 1, "bird": 2, "cat": 3, "deer": 4,
 						"dog": 5, "frog": 6, "horse": 7, "ship": 8, "truck": 9}
@@ -40,18 +25,26 @@ if FINAL_RUN:  # When running on Training Data and untouched Test Data!
 	X_test = utils.get_X("data/test", 300000)
 	y_test = None
 else:  # When running ONLY on Training Data!
-	X_train, X_test, y_train, y_test = cross_validation.train_test_split(X_train, y_train, test_size=0.2)
+	X_train, X_test, y_train, y_test = model_selection.train_test_split(X_train, y_train, test_size=0.2)
 
 
-# Train, Predict, and Store Results with Naive Bayes Model!
-y_test_pred = nb.fit(X_train, y_train).predict(X_test)
+# Train, Predict, and Store Results with Neural Network Model!
+nn = MLPClassifier(
+		activation='relu', algorithm='l-bfgs', alpha=1e-05,
+		batch_size='auto', beta_1=0.9, beta_2=0.999, early_stopping=False,
+		epsilon=1e-08, hidden_layer_sizes=(15,), learning_rate='constant',
+		learning_rate_init=0.001, max_iter=200, momentum=0.9,
+		nesterovs_momentum=True, power_t=0.5, random_state=1, shuffle=True,
+		tol=0.0001, validation_fraction=0.1, verbose=False,
+		warm_start=False)
+y_test_pred = nn.fit(X_train, y_train).predict(X_test)
 utils.y_to_csv(y_test_pred, value_to_class_mapping, "data/testLabels.csv")
 
 
 # Evaluate the best softmax classifier on test set (if results are known)
 if y_test is not None:
 	test_accuracy = np.mean(y_test == y_test_pred)
-	print NAIVE_BAYES_TYPE, "Naive Bayes Test Set Accuracy: ", test_accuracy
+	print "Neural Network Test Set Accuracy: ", test_accuracy
 
 	c_matrix = confusion_matrix(y_test, y_test_pred)
 	print c_matrix
